e008265d5d3c808dcc6f3587d25b9b8440c8fe93
Aitor Gonzalez <12413394+atrvalero@users.noreply.github.com>
2021-05-17 23:19:35 +0000

    V13 - Add icon header

--- js/scripts1621110062.js
+++ js/scripts1621110062.js
@@ -7,7 +7,8 @@
     "use strict"; // Start of use strict
 
     $('.collapsed-menu').append('<div class="p-4 menu-color">\n' +
-        '    <h4 style="color: #717171;">Coneix-nos  ·  #NouImpulsETSEIB <a class="js-scroll-trigger" style="margin-left: 3px;/*color:#1da1f2;*/" href="https://twitter.com/nouimpulsetseib" target="_blank"><i class="fab fa-twitter"></i></a></span></h4>\n' +
+        '    <h4 style="color: #717171;">Coneix-nos  ·  #NouImpulsETSEIB <a class="js-scroll-trigger" style="margin-left: 3px;/*color:#1da1f2;*/" href="https://twitter.com/nouimpulsetseib" target="_blank">' +
+        '    <i class="fab fa-twitter" style="color: #717171;"></i></a></span>  ·  <a href="./assets/programa.pdf" target="_blank"><i class="fas fa-download" style="color: #717171;"></i></a></h4>\n' +
         '    <a class="js-scroll-trigger" href="#candidatura">La Candidatura </a><span>|</span>\n' +
         '    <a class="js-scroll-trigger" href="#equip">El nostre equip. Posa\'ns cara. </a><span>|</span>\n' +
         '    <a class="js-scroll-trigger" href="#presentacio">Presentació </a><span>|</span>\n' +
